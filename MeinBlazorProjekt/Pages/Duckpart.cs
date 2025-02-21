f21eaae59288bc6ab69c5d7b7b97d5c49eee2aad
benjamin.biber <ben.biber.strass@gmail.com>
2025-02-21 09:39:53 +0100

    Added Project

---
+++ MeinBlazorProjekt/Pages/Duckpart.cs
@@ -0,0 +1,10 @@
+﻿namespace Enten.Components.Classes;
+
+public class Duckpart
+{
+    public List<string> Pictures { get; set; }
+    public string Folder { get; set; }
+    public string currentPicture { get; set; }
+    public bool IsVisible { get; set; } = true;
+    public int SortOrder { get; set; }
+}
