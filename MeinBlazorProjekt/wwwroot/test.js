e770cd9bfbe6fbc91c950c274d96913e7e18b153
Benjamin Biber <ben.biber.strass@gmail.com>
2025-04-15 14:03:04 +0200

    Update test.js

--- MeinBlazorProjekt/wwwroot/test.js
+++ MeinBlazorProjekt/wwwroot/test.js
@@ -36,11 +36,6 @@
 
             if (loadedCount === imageUrls.length) {
                 images.forEach(image => {
-                    // Zufälligen Filter auswählen
-                    const filters = ["invert(100%)", "hue-rotate(180)", "sepia(50%)", "none", "none", "none", "none", "none", "none"];
-                    const randomFilter = filters[Math.floor(Math.random() * filters.length)];
-
-                    ctx.filter = randomFilter === "none" ? "none" : randomFilter;
                     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
                 });
             }
